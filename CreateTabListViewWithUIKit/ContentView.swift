2c99d50a0e3b3e42503624d29a6f9124b9e303ca
Rikinyan <karasutengu0319@gmail.com>
2022-10-26 16:33:18 +0900

    Initial Commit

---
+++ CreateTabListViewWithUIKit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CreateTabListViewWithUIKit
+//
+//  Created by 力石優武 on 2022/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CreateTabListViewWithUIKit/CreateTabListViewWithUIKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  CreateTabListViewWithUIKitApp.swift
+//  CreateTabListViewWithUIKit
+//
+//  Created by 力石優武 on 2022/10/26.
+//
+
+import SwiftUI
+
+@main
+struct CreateTabListViewWithUIKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
