2b7ad18c6ac664be537102c5beb60487933b8142
Rikinyan <karasutengu0319@gmail.com>
2022-10-26 19:10:23 +0900

    create pageview

---
+++ CreateTabListViewWithUIKit/TabListView/TabListView.swift
@@ -0,0 +1,73 @@
+//
+//  TabListView.swift
+//  CreateTabListViewWithUIKit
+//
+//  Created by 力石優武 on 2022/10/26.
+//
+
+import SwiftUI
+import UIKit
+
+struct TabListView: View, UIViewControllerRepresentable {
+    
+    func makeCoordinator() -> Coodinator {
+        Coodinator()
+    }
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageViewController.dataSource = context.coordinator
+        
+        pageViewController.setViewControllers([context.coordinator.viewControllers[0]],
+                                              direction: .forward,
+                                              animated: true)
+        
+        return pageViewController
+    }
+    
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+
+    }
+    
+    class Coodinator: NSObject, UIPageViewControllerDataSource {
+        let viewControllers = [
+            UIHostingController(rootView: Text("view 1")),
+            UIHostingController(rootView: Text("view 2")),
+            UIHostingController(rootView: Text("view 3"))
+        ]
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            guard let sampleTextViewController = viewController as? UIHostingController<Text>,
+                  let currentIndex = viewControllers.firstIndex(of: sampleTextViewController) else {
+                return nil
+            }
+            
+            if currentIndex == 0 {
+                return viewControllers.last
+            }
+            
+            return viewControllers[currentIndex - 1]
+        }
+        
+        func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            guard let sampleTextViewController = viewController as? UIHostingController<Text>,
+                  let currentIndex = viewControllers.firstIndex(of: sampleTextViewController) else {
+                return nil
+            }
+            
+            if currentIndex == viewControllers.count - 1 {
+                return viewControllers.first
+            }
+            
+            return viewControllers[currentIndex + 1]
+        }
+        
+        
+    }
+}
+
+struct TabListView_Previews: PreviewProvider {
+    static var previews: some View {
+        TabListView()
+    }
+}
